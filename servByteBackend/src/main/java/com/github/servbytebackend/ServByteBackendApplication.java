91e94489d1b69cec5b60f36a646e1c2d191fbfa7
Oluwatobi Jolayemi <jolayemi.tobi@gmail.com>
2022-05-17 10:59:29 +0100

    Initial project setup

---
+++ servByteBackend/src/main/java/com/github/servbytebackend/ServByteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.servbytebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServByteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServByteBackendApplication.class, args);
+    }
+
+}
